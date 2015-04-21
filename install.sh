55ae0a4096df28c622eccfb5c13a2d8b2e6d4dd1
Jason Gedge <jason@gedge.ca>
2015-04-20 21:36:12 -0400

    Add some nice output for the install script.

--- install.sh
+++ install.sh
@@ -3,22 +3,33 @@
 # If dotfiles don't already exist, clone repo
 # TODO perhaps check pwd to see if some files exist so that one could instead clone into
 #      into a different directory other than $HOME/dotfiles
-if [ ! -e "$HOME/dotfiles" ]
-then
+if [ ! -e "$HOME/dotfiles" ]; then
+	echo -e "---> \e[92mFetching dotfiles repo...\e[0m"
     git clone 'https://github.com/thegedge/dotfiles.git' $HOME/dotfiles
 fi
 
 # Symbolic links to the dotfiles
+echo -e "---> \e[92mLinking dotfiles...\e[0m"
 FILES=(profile gitconfig gitignore_global tmux.conf vimrc zprofile zshrc vim)
-for fname in "${FILES[@]}"
-do
-    [ ! -e $HOME/.$fname ] && ln -s $HOME/dotfiles/$fname $HOME/.$fname
+for fname in "${FILES[@]}"; do
+    if [ ! -e $HOME/.$fname ]; then
+		echo "Linking .$fname"
+    	ln -s $HOME/dotfiles/$fname $HOME/.$fname
+    fi
 done
 
 # Install oh-my-zsh (zsh plugins and such)
-[ ! -e "$HOME/.oh-my-zsh" ] && curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-[ -e "$HOME/.zshrc.pre-oh-my-zsh" ] && mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
+if [ ! -e "$HOME/.oh-my-zsh" ]; then
+	echo -e "---> \e[92mFetching and installing oh-my-zsh...\e[0m"
+	curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+	[ -e "$HOME/.zshrc.pre-oh-my-zsh" ] && mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
+fi
 
 # Install vundle (vim plugin manager)
-[ ! -e "$HOME/.vim/bundle/Vundle.bim" ] && git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -e "$HOME/.vim/bundle/Vundle.vim" ]; then
+	echo -e "---> \e[92mFetching and installing Vundle...\e[0m"
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+echo -e "---> \e[92mInstalling vundle plugins...\e[0m"
 vim +PluginInstall +qall
